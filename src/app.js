78b89c8b1ae443d5c1f4ce1886f140b21bf22917
Prasad Kanakgiri <kanakgiriprasad26@gmail.com>
2024-06-16 12:59:00 +0530

    adding register controller and fixing bugs

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({extended: true, limit:"16kb"}))  // this is used to 
 app.use(express.static("public"))  // this is used to store files in image, pdf format
 app.use(cookieParser())
 
-// route import
+// route import and route declaration
 import userRouter from './routes/user.routes.js'
-// route declaration
 app.use("/api/v1/users", userRouter)
+
 export {app}
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,78 @@
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import { ApiError } from "../utils/ApiError.utils.js"
+import { User} from "../models/user.models.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.utils.js"
+import { ApiResponse } from "../utils/ApiRsponse.utils.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "ok"
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+    const {fullname, email, username, password} = req.body
+    console.log("Email: ", email);
+
+    // validation
+    if(
+        [fullname, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fiels are required")
+    }
+
+    // heck if user already exists: username, email
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     })
+    if(existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    // check for images, check for avatar
+    // below expression req.files?.avatar[0]?.path attempts to access the path of the first file uploaded under the avatar field.
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    // upload them to cloudinary, avatar
+    const avatar = uploadOnCloudinary(avatarLocalPath);
+    const coverImage = uploadOnCloudinary(coverImageLocalPath);
+
+    // check if avatar file is uploaded because it is required field
+    if(!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    // create user object - create entry in db
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email, 
+        password,
+        username: username.toLowerCase()
+    })
+
+    // remove password and refresh token field from response
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    // send response
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully !!!")
+    )
 })
 
 export {registerUser}
